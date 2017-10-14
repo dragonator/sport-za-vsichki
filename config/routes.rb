2597e162f8583e0ce714aba0c801524910087b67
Teodor Draganov <draganov93@gmail.com>
2017-10-14 11:44:43 +0300

    Show register view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :events
   resources :coaches
   resources :users
+  resources :register
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#not_ready'
 end
